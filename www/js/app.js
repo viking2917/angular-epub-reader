5086743ecb421c20be09d44fd3e568b13463bbb7
Mark Watkins <mark.watkins1@gmail.com>
2019-03-15 12:01:06 -1000

    Fix incorrect / obsolete call to keyboard plugin

--- www/js/app.js
+++ www/js/app.js
@@ -13,9 +13,10 @@ angular.module('starter', ['ionic', 'epubreader'])
     // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
     // useful especially with forms, though we would prefer giving the user a little more room
     // to interact with the app.
-    if (window.cordova && window.Keyboard) {
-      window.Keyboard.hideKeyboardAccessoryBar(true);
-    }
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+	    cordova.plugins.Keyboard.hideFormAccessoryBar(true);
+	    cordova.plugins.Keyboard.disableScroll(true);
+	  }
 
     if (window.StatusBar) {
       // Set the statusbar to use the default style, tweak this to
